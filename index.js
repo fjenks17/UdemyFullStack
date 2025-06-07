e3a5ee1ccba3c3ac906e7cb8c6e9e0223a28963e
fjenks17 <fjenks17@gmail.com>
2025-06-07 11:44:07 -0400

    final i hope

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ var rand1 = Math.ceil(Math.random()*6);
 var rand2 = Math.ceil(Math.random()*6);
 document.getElementsByClassName("img1")[0].setAttribute("src", `images/dice${rand1}.png`);
 document.getElementsByClassName("img2")[0].setAttribute("src", `images/dice${rand2}.png`);
+
+var header = document.getElementsByTagName("h1")[0];
+if(rand1 > rand2){
+    header.innerHTML = "Player 1 Wins!";
+}
+else if(rand2 > rand1){
+    header.innerHTML = "Player 2 Wins!"
+}
+else{
+    header.innerHTML = "Draw!";
+}
