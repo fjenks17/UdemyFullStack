b825eff4d339a51e295062cc5e27125bb9400105
fjenks17 <fjenks17@gmail.com>
2025-06-07 11:32:09 -0400

    isuck

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var rand1 = Math.ceil(Math.random()*6);
 var rand2 = Math.ceil(Math.random()*6);
-document.getElementsByClassName("img1").setAttribute("src", `/images/dice${rand1}.png`);
-document.getElementsByClassName("img2").setAttribute("src", `/images/dice${rand2}.png`);
+document.getElementsByClassName("img1")[0].setAttribute("src", `images/dice${rand1}.png`);
+document.getElementsByClassName("img2")[0].setAttribute("src", `images/dice${rand2}.png`);
